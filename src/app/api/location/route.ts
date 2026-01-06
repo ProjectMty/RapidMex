5d90486c0b22e17e45fb24f0c2760c004eabe395
ailin-cantu <it03@cargomty.com>
2026-01-06 17:02:30 -0600

    ubicacion al ingresar

---
+++ src/app/api/location/route.ts
@@ -0,0 +1,36 @@
+
+import { NextResponse } from 'next/server';
+
+
+export async function POST(req: Request) {
+
+    try {
+
+        const body = await req.json();
+        const { latitud, longitud, fecha } = body;
+
+        const response = await fetch(
+            "https://script.google.com/macros/s/AKfycbzwJuP4cALcS2YFLBbdopVgdVXSv0PuiFAbf97NGvGvmilQAAf_QD-5jOV2JdUS6-vb/exec",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    latitud,
+                    longitud,
+                    fecha
+                }),
+            }
+        );
+
+        return NextResponse.json({ ok: true, response })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { ok: false, error: "Error enviando datos a Google Apps Script" },
+            { status: 500 }
+        );
+    }
+}
--- src/app/usa-mex/page.tsx
+++ src/app/usa-mex/page.tsx
@@ -4,18 +4,19 @@
 import Hero from "@/components/usa-mex/hero";
 import Costos from "@/components/usa-mex/costos";
 import CostosHD from "@/components/usa-mex/costo-hd";
-
 import Cajas from "@/components/usa-mex/cajas";
 import Ruleta from "@/components/usa-mex/ruleta";
 import ContactUsa from "@/components/usa-mex/Contact-usa";
 import FooterUsa from "@/components/usa-mex/Footer-usa";
 import NavbarUsa from "@/components/usa-mex/Navbar-usa";
+import Ubicacion from "@/components/usa-mex/ubicacion";
 export default function CotizadorPage() {
 
   return (
     <main>
+      <Ubicacion />
       <NavbarUsa />
-      <Ruleta />
+      {/* <Ruleta /> */}
       <Hero />
       <Costos />
       <Cajas />
